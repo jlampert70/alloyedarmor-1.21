c6335b223d199cbc29ee15e920427434c600098d
Jobin Lampert <jlampert70@student.oprfhs.org>
2025-05-07 16:38:38 -0500

    Initial commit

---
+++ src/main/java/com/weaklink/alloyedarmor/AlloyedArmorClient.java
@@ -0,0 +1,10 @@
+package com.weaklink.alloyedarmor;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AlloyedArmorClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/weaklink/alloyedarmor/AlloyedarmorDataGenerator.java
@@ -0,0 +1,11 @@
+package com.weaklink.alloyedarmor;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AlloyedarmorDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
